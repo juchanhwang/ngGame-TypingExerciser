a989af0b81d886a09b6caf7cd6e0f9b9a9a24af5
juchanhwang <ghkdwncks96@gmail.com>
2019-09-27 20:10:44 +0900

    refactor

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
   wordListUrl = 'http://localhost:3000/wordlist';
-  constructor (private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getWordList() {
-    return this.http.get(this.wordListUrl);
+    debugger;
+    return this.http.get(this.wordListUrl)
   }
 }
